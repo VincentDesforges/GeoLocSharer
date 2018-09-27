faabd8b781ff0441168d447463bce9fe08a0b14d
Vincent Desforges <desforges.vincent@gmail.com>
2018-09-27 21:26:35 +0100

    set useCreateIndex to true in mongoose to avoid using deprecated ensureIndex method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/GeoLocSharerApp', {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true); // avoid using the deprecated ensureIndex method
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/GeoLocSharerApp', { useNewUrlParser: true });
 
 module.exports = { mongoose };
